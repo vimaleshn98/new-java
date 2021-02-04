f2d4c7cb1854886198f6210ff7bd2309a4aef650
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-04 18:03:46 +0530

    f1 is loop

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//change ds
+		int[] arr = {2,4,3,5,3,2,2};
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
