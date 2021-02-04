02b11f076337fd2524dd374d06533b6ce69f649e
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-04 17:57:18 +0530

    master using while loop

--- src/App.java
+++ src/App.java
@@ -4,6 +4,11 @@ class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//change ds
+		int[] arr = { 1, 2, 4 ,5};
+		int i=0;
+		while(i>arr.length) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
